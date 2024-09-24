34df7213c0d0d18c6d2753ea0fe253197efa4684
BenjiD21 <benni@daur.de>
2024-09-24 18:45:59 +0200

    Edit2

--- EntraID/GetNewlyCreatedGuestAccounts.ps1
+++ EntraID/GetNewlyCreatedGuestAccounts.ps1
@@ -1,4 +1,5 @@
 # Connect to Graph
+Connect-MgGraph -Scopes "Directory.Read.All"
 
 # Create the ArrayList that should later be exported
 $exportTable = New-Object System.Collections.ArrayList
@@ -11,16 +12,60 @@ foreach ($role in $roles)
   $roleObjectGet = Get-MgRoleManagementDirectoryRoleAssignment -Filter "roleDefinitionId eq '$($role.Id)'"
   
   #Get Username from PrincipalID parameter
-  if ($roleObjectGet -ne $null) {
-    $userPrincipal = Get-MgUser -UserID roleObjectGet.principalID
-    $userPrincipal.UserPrincipalName
+  if ($roleObjectGet.PrincipalId) {
+        
+    #check if the variable contains more than one PrincipalId
+    if($roleObjectGet.PrincipalId.Count) {
+        
+        #if it does - make it an array by splitting " "
+        try {
+            $roleObjectGetArray = $roleObjectGet.PrincipalId -split " "
+
+            #Work through the array and get the corresponding user as well as role
+            $roleObjectGetArray | ForEach-Object {
+
+                #Check if the Object is a user or service principal <- creates Exception if there is a GDAP role group assignment FIX
+                $directoryObject = Get-MgDirectoryObject -DirectoryObjectId $_
+
+                if ($directoryObject.additionalproperties.("@odata.type") -eq "#microsoft.graph.servicePrincipal") {
+                    $principal = $directoryObject.additionalproperties.displayName
+                    $principal
+
+                    $roleName = $role.DisplayName
+                    $roleName
+                } elseif ($directoryObject.additionalproperties.("@odata.type") -eq "#microsoft.graph.user") {
+                    $userPrincipalName = $directoryObject.additionalproperties.userPrincipalName
+                    $userPrincipalName
+
+                    $roleName = $role.DisplayName
+                    $roleName
+                } elseif ($directoryObject.additionalproperties.("@odata.type") -eq "#microsoft.graph.group") {
+                    $principal = $directoryObject.additionalproperties.displayName
+                    $principal
+
+                    $roleName = $role.DisplayName
+                    $roleName
+                }
+
+            }
+        } catch [System.Exception] {
+            Write-Output("Unbekannter Objekttyp")
+        }
+        
+    } else {
+
+        #If there is only one user assigned to the role, do the same as above
+        $userPrincipal = Get-MgUser -UserID "$($roleObjectGet.PrincipalId)"
+        $userPrincipal.UserPrincipalName
+
+        $roleName = $role.DisplayName
+        $roleName
+    }
+
   }
 
-  #Get Rolename from roleDefinitionID parameter
-  if ($roleObjectGet -ne $null) {
-    $roleName = get-mgdirectoryrole -Filter "RoleTemplateId eq '$roleObjectGet.RoleDefinitionId'"
-    $roleName.DisplayName
-  }
+  #Set the $userprincipal to $null again
+  $userPrincipal = $null
 
   $exportTable.add($roleObjectGet)
 }
